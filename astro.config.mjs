121d8ff89d17b01132bc0fdefe0496dbb84cde00
Danny Smith <hi@danny.is>
2023-02-24 23:49:33 +0000

    Add Astro image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
