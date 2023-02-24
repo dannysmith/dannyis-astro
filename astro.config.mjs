cae23831bcc0ac178ffcb81e63cc6374e54ee79a
Danny Smith <hi@danny.is>
2023-02-24 23:56:29 +0000

    Clean up astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
