8028b4dc10116149c70a00a266ccb7bd2d0da2f8
Danny Smith <hi@danny.is>
2023-02-26 17:44:21 +0000

    Consolidate Article components
    So they can be imported from one file

---
+++ src/components/article.ts
@@ -0,0 +1,6 @@
+// Use this file to import and export all components which are likely to be used in MDX files
+
+import Article from './Article.astro';
+import Notion from './Notion.astro';
+
+export {Article, Notion};
