30e4d65a290e4dc2570422f854a1c41c6661cc51
Danny Smith <hi@danny.is>
2023-02-26 15:13:07 +0000

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,12 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://danny.is",
-  integrations: [mdx(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), prefetch(), sitemap()]
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+    prefetch(),
+    sitemap()
+  ],
 });
