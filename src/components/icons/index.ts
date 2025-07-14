327fd205cd9428dd401ba5cfe0a35d778aa41062
Danny Smith <hi@danny.is>
2025-07-14 04:03:39 +0100

    Add icons index

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as RSSIcon } from './RSSIcon.astro';
+export { default as InstagramIcon } from './InstagramIcon.astro';
+export { default as LinkedInIcon } from './LinkedInIcon.astro';
+export { default as NotionIcon } from './NotionIcon.astro';
+export { default as YouTubeIcon } from './YouTubeIcon.astro';
