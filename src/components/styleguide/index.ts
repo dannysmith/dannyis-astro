873ec8ee9d92cf8fe4d0f0045ed67f661ca53014
Danny Smith <hi@danny.is>
2026-01-26 01:50:23 +0000

    Update Spacing System Styleguide

--- src/components/styleguide/index.ts
+++ src/components/styleguide/index.ts
@@ -1,7 +1,6 @@
 // Barrel export for styleguide-specific components
 // These are helpers for the styleguide that aren't useful elsewhere
 
-import SpacingScale from './SpacingScale.astro';
 import StyleguideTOC from './StyleguideTOC.astro';
 
-export { SpacingScale, StyleguideTOC };
+export { StyleguideTOC };
