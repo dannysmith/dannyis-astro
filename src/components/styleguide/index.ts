a95fcc6b1e8040a68a603d48c124f01ca802b47d
Danny Smith <hi@danny.is>
2026-01-26 04:51:06 +0000

    Add typography switcher

--- src/components/styleguide/index.ts
+++ src/components/styleguide/index.ts
@@ -1,6 +1,7 @@
 // Barrel export for styleguide-specific components
 // These are helpers for the styleguide that aren't useful elsewhere
 
+import SGTypographySwitcher from './SGTypographySwitcher.astro';
 import StyleguideTOC from './StyleguideTOC.astro';
 
-export { StyleguideTOC };
+export { SGTypographySwitcher, StyleguideTOC };
